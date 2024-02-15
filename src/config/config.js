c64a3d7cd13884d7fffd2aef9bdb383f13560f4a
Art96em <artem-w0lf@yandex.ru>
2024-02-15 17:26:57 +0200

    update env variables

--- src/config/config.js
+++ src/config/config.js
@@ -1,6 +1,8 @@
 import Connection from "../util/Connection";
 
-const host = process.env.SERVER_HOST || 'localhost'
+const host = process.env.SERVER_HOST || '146.148.98.64'
 const port = process.env.SERVER_PORT || 4000
+
+console.log(`host is: ${host}; port is: ${port}`);
 
 export const connection = new Connection(`http://${host}:${port}`)
